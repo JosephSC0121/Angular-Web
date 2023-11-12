6da414f978760835c196c5cbff5fcf4fe8780fe4
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2023-11-12 17:42:00 -0500

    All services

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,27 +1,42 @@
-import {Component, OnInit} from '@angular/core';
-import {UserService} from "../../domain/users/user.service";
-import {UserModel} from "../../domain/users/user.model";
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../domain/users/user.service";
+import { UserModel } from "../../domain/users/user.model";
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit{
+export class UserComponent implements OnInit {
 
-  listUsers: UserModel[] = [];
-  constructor(private userService: UserService) {
+  listUsers$: Observable<UserModel[]>;
 
+  constructor(private userService: UserService, private router: Router) {
+    this.listUsers$ = this.userService.getAllUser();
   }
+
   ngOnInit(): void {
-    this.userService.getAllUser().subscribe(data => {
-      this.listUsers = data;
+    this.refreshUserList();
+  }
+
+  deleteUser(userId: number): void {
+    this.userService.deleteUser(userId).subscribe(() => {
+      this.refreshUserList();
     });
   }
-  deleteUser(userId: number): void {
-    this.userService.deleteUser(userId).subscribe(data => {
-      this.ngOnInit()
-    });
-}
+
+  private refreshUserList(): void {
+    this.listUsers$ = this.userService.getAllUser();
+  }
+
+  navigateToCreateUser(): void {
+      this.router.navigate(['component/createUser']);
+    
+  }
+  navigateToUpdateUser(userId?: number): void{
+    this.router.navigate(['component/updateUser', {userId}]);
+  }
 
 }
